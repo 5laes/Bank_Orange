296323fb436462edd1e97d2d3f35bca96882e28b
5laes <claeslundin@pm.me>
2022-11-22 14:36:44 +0100

    Add project files.

---
+++ Bank_Orange/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Bank_Orange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public void StartProgram()
+        {
+
+        }
+    }
+}
