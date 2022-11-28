d0ba34b391d6d123b539b2c962709a3c910318d1
Filip Mossberg <filip.mossberg@gmail.com>
2022-11-28 09:24:44 +0100

    LoginAttempts

--- Bank_Orange/BankSystem.cs
+++ Bank_Orange/BankSystem.cs
@@ -10,5 +10,20 @@ namespace Bank_Orange
 
         private Dictionary<int, Person> PersonDictionary = new Dictionary<int, Person>();
         private Dictionary<int, BankAccount> AccountDictionary = new Dictionary<int, BankAccount>();
+
+        public void Login()
+        {
+            for (int Attempts = 0; Attempts <= 3; Attempts++)
+            {
+                if (Attempts < 3)
+                {
+                    
+                }
+                else
+                {
+                    Environment.Exit(0);
+                }
+            }
+        }
     }
 }
