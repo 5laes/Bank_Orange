ddbdc18619226310d4c029d1a8cab33af6fe8868
5laes <claeslundin@pm.me>
2022-11-28 09:41:00 +0100

    Added values to person and accountdetails and created methods to create a user and admin.

--- Bank_Orange/AccountDetails.cs
+++ Bank_Orange/AccountDetails.cs
@@ -6,5 +6,24 @@ namespace Bank_Orange
 {
     class AccountDetails
     {
+        private string accountName;
+        public string AccountName
+        {
+            get { return accountName; }
+            set { accountName = value; }
+        }
+
+        private decimal money;
+        public decimal Money
+        {
+            get { return money; }
+            set { money = value; }
+        }
+
+        public AccountDetails(string accountName, decimal money)
+        {
+            AccountName = accountName;
+            Money = money;
+        }
     }
 }
--- Bank_Orange/Person.cs
+++ Bank_Orange/Person.cs
@@ -6,5 +6,25 @@ namespace Bank_Orange
 {
     class Person
     {
+        private string name;
+        public string Name 
+        { 
+            get { return name; } 
+            set { name = value; }
+        }
+
+        private string password;
+        public string Password 
+        { 
+            get { return password; }
+            set { password = value; }
+        }
+
+        private bool isAdmin;
+        public bool IsAdmin 
+        { 
+            get { return isAdmin; }
+            set { isAdmin = value; }
+        }
     }
 }
