8970dfdda449c4c5b50da64f551cf9ab76f5e45f
Claes Lundin <claeslundin@pm.me>
2022-12-07 13:33:45 +0100

    fix

--- Bank_Orange/PendingTransactions.cs
+++ Bank_Orange/PendingTransactions.cs
@@ -6,6 +6,13 @@ namespace Bank_Orange
 {
     internal class PendingTransactions
     {
+        public BankAccount Receiver;
+        public decimal Money;
 
+        public PendingTransactions(BankAccount receiver, decimal money)
+        {
+            Receiver = receiver;
+            Money = money;
+        }
     }
 }
